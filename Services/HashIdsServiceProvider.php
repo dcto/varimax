23c8c61ea4aeb67ba9bf08e6076df0d50ead819a
11 <sdoz@live.com>
2020-08-28 19:31:05 +0800

    Addition ID module

---
+++ Services/HashIdsServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace VM\Services;
+
+/**
+ * Varimax The Full Stack PHP Frameworks.
+ * varimax.cn
+ * FILE: 2020
+ * USER: 陶之11. <sdoz@live.com>
+ * Time: 2020-08-19 16:53
+ * SITE: https://www.varimax.cn/
+ */
+
+use Hashids\Hashids;
+
+class HashIdsServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if(is_file($config = root('config', 'id.php'))) {
+            $this->mergeConfigFrom($config, "id");
+        }
+
+        $this->app->singleton('id', function () {
+            return new Hashids(config('id.key',config('app.key', 'VM:')), config('id.len', 16), config('id.bet', join('', range(0,9)).join('',range('a','z'))));
+        });
+
+    }
+}
