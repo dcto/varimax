1e2852a939d986c4ba8130f7fb3b405072bc0484
陶之11 <DC@DC.local>
2020-09-11 19:28:10 +0800

    addition ServiceProvider Support

---
+++ src/Support/ServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace VM\Support;
+
+/**
+ * Varimax The Full Stack PHP Frameworks.
+ * varimax.cn
+ * FILE: 2020
+ * USER: 陶之11. <sdoz@live.com>
+ * Time: 2020-08-19 16:53
+ * SITE: https://www.varimax.cn/
+ */
+
+abstract class ServicesProvider extends \Illuminate\Support\ServiceProvider{}
