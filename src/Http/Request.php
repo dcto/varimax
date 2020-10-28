359557ba449f5861f593404892c2d720385091be
陶之11 <DC@DC.local>
2020-10-28 14:51:34 +0800

    addition mobile and weixin method to Request module

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -494,6 +494,45 @@ class Request extends HttpFoundation\Request implements Arrayable, \ArrayAccess
         );
     }
 
+    /*
+    * 判断是否为手机
+    * @return bool
+    */
+    public function mobile(){ 
+        // 如果有HTTP_X_WAP_PROFILE则一定是移动设备
+        if ($this->server('HTTP_X_WAP_PROFILE')){
+            return true;
+        } 
+        // 如果via信息含有wap则一定是移动设备,部分服务商会屏蔽该信息
+        if ($this->server('HTTP_VIA')){ 
+            // 找不到为flase,否则为true
+            return stristr($_SERVER['HTTP_VIA'], 'wap') ? true : false;
+        } 
+        // 脑残法，判断手机发送的客户端标志,兼容性有待提高
+        if ($this->server('HTTP_USER_AGENT')){
+            $clientkeywords = array ('nokia','sony','ericsson','mot','samsung','htc','sgh','lg','sharp','sie-','philips','panasonic','alcatel','lenovo','iphone','ipod','blackberry','meizu','android','netfront','symbian','ucweb','windowsce','palm','operamini','operamobi','openwave','nexusone','cldc','midp','wap','mobile'); 
+            // 从HTTP_USER_AGENT中查找手机浏览器的关键字
+            if (preg_match("/(" . implode('|', $clientkeywords) . ")/i", strtolower($this->server('HTTP_USER_AGENT')))){
+                return true;
+            } 
+        } 
+        // 协议法，因为有可能不准确，放到最后判断
+        if ($this->server('HTTP_ACCEPT')){ 
+            // 如果只支持wml并且不支持html那一定是移动设备
+            // 如果支持wml和html但是wml在html之前则是移动设备
+            if ((strpos($this->server('HTTP_ACCEPT'), 'vnd.wap.wml') !== false) && (strpos($this->server('HTTP_ACCEPT'), 'text/html') === false || (strpos($this->server('HTTP_ACCEPT'), 'vnd.wap.wml') < strpos($this->server('HTTP_ACCEPT'), 'text/html')))){
+                return true;
+            } 
+        } 
+        return false;
+    }
+    
+    /**
+     * 判断是否为微信
+     */
+    public function weixin() { 
+        return strpos($this->server('HTTP_USER_AGENT'), 'MicroMessenger') !== false;
+    }
 
     /**
      * query get QueryString
