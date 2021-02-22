a5afe6d62fdfbfd0469123011e7630f57ea4f8e8
dcto <sdoz@live.com>
2021-02-22 15:51:33 +0800

    remove the sbc dbc function

--- src/Support/Function.php
+++ src/Support/Function.php
@@ -525,41 +525,51 @@ if(!function_exists('abort')) {
         app()->abort($code, $message, $headers);
 }
 
-if(!function_exists('sbc2dbc')){
+if(!function_exists('unicode2char')){
     /**
-    * 全角转半角
-    * @param string $str
-    * @return string
-    **/
-    function sbc2dbc($str){
-        return preg_replace(
-        // 全角字符 
-        '/[\x{3000}\x{ff01}-\x{ff5f}]/ue',
-        // 编码转换
-        // 0x3000是空格，特殊处理，其他全角字符编码-0xfee0即可以转为半角
-        '($unicode=char2Unicode(\'\0\')) == 0x3000 ? " " : (($code=$unicode-0xfee0) > 256 ? unicode2Char($code) : chr($code))',
-        $str
-        );
+     * 将unicode转换成字符
+     * @param int $unicode
+     * @return string UTF-8字符
+     **/
+    function unicode2char($unicode){
+        if($unicode < 128)     return chr($unicode);
+        if($unicode < 2048)    return chr(($unicode >> 6) + 192) .
+                                      chr(($unicode & 63) + 128);
+        if($unicode < 65536)   return chr(($unicode >> 12) + 224) .
+                                      chr((($unicode >> 6) & 63) + 128) .
+                                      chr(($unicode & 63) + 128);
+        if($unicode < 2097152) return chr(($unicode >> 18) + 240) .
+                                      chr((($unicode >> 12) & 63) + 128) .
+                                      chr((($unicode >> 6) & 63) + 128) .
+                                      chr(($unicode & 63) + 128);
+        return false;
+    }
+}
+if(!function_exists('char2unicode')){
+    /**
+     * 将字符转换成unicode
+     * @param string $char 必须是UTF-8字符
+     * @return int
+     **/
+    function char2unicode($char){
+        switch (strlen($char)){
+            case 1 : return ord($char);
+            case 2 : return (ord($char{1}) & 63) |
+                            ((ord($char{0}) & 31) << 6);
+            case 3 : return (ord($char{2}) & 63) |
+                            ((ord($char{1}) & 63) << 6) |
+                            ((ord($char{0}) & 15) << 12);
+            case 4 : return (ord($char{3}) & 63) |
+                            ((ord($char{2}) & 63) << 6) |
+                            ((ord($char{1}) & 63) << 12) |
+                            ((ord($char{0}) & 7)  << 18);
+            default :
+                trigger_error('Character is not UTF-8!', E_USER_WARNING);
+                return false;
+        }
     }
 }
 
-if(!function_exists('dbc2sbc')){
-    /**
-    * 半角转全角
-    * @param string $str
-    * @return string
-    **/
-    function dbc2sbc($str){
-        return preg_replace(
-            // 半角字符 
-            '/[\x{0020}\x{0020}-\x{7e}]/ue',  
-            // 编码转换
-            // 0x0020是空格，特殊处理，其他半角字符编码+0xfee0即可以转为全角
-            '($unicode=char2Unicode(\'\0\')) == 0x0020 ? unicode2Char（0x3000） : (($code=$unicode+0xfee0) > 256 ? unicode2Char($code) : chr($code))',
-            $str
-        );
-    }
-}
 
 if(!function_exists('is_phone')) {
     /**  
