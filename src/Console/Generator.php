6feb2b0890588563c2e6755f1065f8665041752d
dc.To <sdoz@live.com>
2023-08-06 16:18:37 +0800

    update Generator

--- src/Console/Generator.php
+++ src/Console/Generator.php
@@ -1,10 +1,24 @@
 <?php
+/**
+* 
+* @package varimax
+* @author  dc.To
+* @version 20230806
+* @copyright ©2023 dc team all rights reserved.
+*/
+namespace VM\Console;
 
-namespace VM\Develop;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Class Generator
- * @package VM\Develop
+ * @package VM\Console
  */
-class Generator{
+class Generator extends Command {
+
+
 }
