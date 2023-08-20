be9792ee1e9edd1b6b15ad6c548d175bc6a3f704
dc.To <sdoz@live.com>
2023-08-20 14:45:15 +0800

    update Exception system

--- src/Exception/E.php
+++ src/Exception/E.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Varimax The Slim PHP Frameworks.
  * varimax
@@ -18,7 +19,7 @@ class E {
      *
      * @var integer
      */
-    static private $debug = 2;
+    static private $debug = 0;
 
     /**
      * 备用内存大小
@@ -31,7 +32,7 @@ class E {
      */
     static public function register()
     {
-        if(self::$debug = getenv('DEBUG')){
+        if(static::$debug = getenv('DEBUG')){
             //错误级别
             error_reporting(E_ALL);
             //开启错误
@@ -39,22 +40,16 @@ class E {
         }
 
         //预留内存
-        self::$memory && str_repeat('*', self::$memory);
-
-        //截获未捕获的异常
-        set_exception_handler(function($e){
-             self::onException($e);
-        });
+        static::$memory && str_repeat('*', static::$memory);
 
         //截获各种错误
-        set_error_handler(function($code, $message, $file, $line){
-            self::onError($code, $message, $file, $line);
-        });
+        set_error_handler(array(__CLASS__,'onError'));
+
+        //截获未捕获的异常
+        set_exception_handler(array(__CLASS__,'onException'));
 
         //截获致命性错误
-        register_shutdown_function(function(){
-            self::onShutdown();
-        });
+        // register_shutdown_function([__CLASS__,'onShutdown']);
     }
 
     /**
@@ -73,8 +68,8 @@ class E {
      */
     static public function onException($e)
     {
-         self::logException($e);
-         self::display($e);
+        static::logException($e);
+        static::display($e);
     }
 
     /**
@@ -87,12 +82,9 @@ class E {
      */
     static public function onError($code, $message, $file, $line)
     {
-        self::$memory = null;
-
-        //将错误变成异常抛出 统一交给异常处理函数进行处理
-        if(error_reporting() & $code) {
-            self::onException(new \ErrorException($message, $code, 1, $file, $line));
-        }
+       static::$memory = null;
+       static::onException(new \ErrorException($message, $code, 1, $file, $line));
+        
     }
 
     /**
@@ -101,11 +93,11 @@ class E {
     static public function onShutdown()
     {
         //释放备用内存供下面处理程序使用
-        self::$memory = null;
+       static::$memory = null;
 
         //最后一条错误信息
         if(is_null($e = error_get_last()) === false) {
-            self::onError($e['type'], $e['message'], $e['file'], $e['line']);
+           static::onError($e['type'], $e['message'], $e['file'], $e['line']);
         }
     }
 
@@ -114,28 +106,32 @@ class E {
      * 获取异常调用
      *
      * @param $code
-     * @param $e
-     * @return string
+     * @param $e \Exception|Object
+     * @return array
      */
-    final static function debugBacktrace(\Exception $e)
+    final static function debugBacktrace($e)
     {
         /**
          * @var $e \Exception
          */
         return array_map(function($trace){ 
             isset($trace['class']) && $trace['function'] = $trace['class'].$trace['type'].$trace['function'];
-            $trace['function'] .= '('.join(', ' , array_map(function($arg){
-                if(in_array(gettype($arg),['array', 'object', 'boolean'])){
-                    if($arg instanceof \Closure) return '\Closure';
-                    if(is_object($arg)) return get_class($arg);
-                    if(is_bool($arg)) return $arg ? 'True' : 'False';
-                    return json_encode($arg, true);
-                }else{
-                    return is_string($arg) ? "'".$arg."'" : $arg;
-                }
-            }, $trace['args'])).')';
-            return array('file' => str_replace(_DOC_,'', isset($trace['file']) ? $trace['file'] : '\Closure'), 'line' => isset($trace['line']) ? $trace['line'] : 0, 'function' => $trace['function']);
-        },$e->getTrace());
+            if(isset($trace['args'])){
+                $trace['function'] .= '('.join(', ' , array_map(function($arg){
+
+                    if(in_array(gettype($arg),['array', 'object', 'boolean'])){
+                       
+                        if(is_object($arg)) return get_class($arg);
+                        if(is_bool($arg)) return $arg ? 'True' : 'False';
+                        return json_encode($arg, true);
+                    }else{
+                        return is_string($arg) ? "'".$arg."'" : $arg;
+                    }
+                }, $trace['args'])).')';
+            }
+            return array('file' => str_replace(_DOC_,'', isset($trace['file']) ? $trace['file'] : ''), 'line' => isset($trace['line']) ? $trace['line'] : 0, 'function' => $trace['function']);
+        
+        }, $e->getTrace());
     }
 
     /**
@@ -179,29 +175,17 @@ class E {
         }else{
             ob_get_contents() && ob_end_clean();
             http_response_code($e instanceof Exception ? $e->getStatus() : 500);
-
-            if (self::$debug == 2) {
-                $debugBacktrace = self::debugBacktrace($e);
-                    echo '<html><head><title>' . $e->getMessage() . '</title><meta name="robots" content="none" /><style type="text/css">body {font: 12pt verdana; margin: 10px auto;}div {background: #f5f5f5; border-radius: 5px; line-height: 200%; margin-bottom: 1em; padding: 1em;}table {background: #aaa;}.stack {background-color: #ffc;}.title {background-color: #eee;}</style></head><body><div id="title"><b>' . Exception::error($e->getCode()) . '</b>: ' . $e->getMessage() . '</div>';
-                    if ($debugBacktrace) {
-                        echo '<div id="debug"><p><b>Debug Backtrace &copy;Varimax</b></p><table cellpadding="5" cellspacing="1" width="100%" class="table"><tbody>';
-                        if (is_array($debugBacktrace)) {
-                            echo '<tr class="title"><td>No.</td><td>File</td><td>Line</td><td>Code</td></tr>';
-                            foreach ($debugBacktrace as $k => $error) {
-                                $k++;
-                                echo "<tr class=\"stack\"><td>{$k}</td><td>{$error['file']}</td><td>{$error['line']}</td><td>{$error['function']}</td></tr>";
-                            }
-                        } else {
-                            echo '<tr><td><ul>' . $debugBacktrace . '</ul></td></tr>';
-                        }
-                        echo '</tbody></table></div>';
-                    }
-                    echo '</body></html>';
-
-            }else if(self::$debug == 1){
-                die($e->getMessage());
+            static::$debug == 1 && die($e->getMessage());
+            if(static::$debug == 2) {
+                if(is_array ($debugBacktrace = static::debugBacktrace($e))){
+                    $c = count($debugBacktrace);
+                    $debugBacktrace = '<tr bgcolor="#eee"><td>No.</td><td>File</td><td>Line</td><td>Code</td></tr>'. join('', array_map(function($v) use(&$c){
+                            return vsprintf('<tr bgcolor="#ffc"><td>'.$c--.'</td><td>%s</td><td>%d</td><td>%s</td></tr>', $v);}, $debugBacktrace));
+                }else{
+                    $debugBacktrace = "<tr><td><ul>{$debugBacktrace}</ul></td></tr>";
+                }
+                echo str_replace(['$error', '$file', '$title', '$line', '$backtrace'], [Exception::error($e->getCode()),  $e->getFile(), $e->getMessage(), $e->getLine(), $debugBacktrace], '<html><head><title>$title</title></head><body style="background: #eee; padding: 1em;"><div><p><b>File</b>: $file (Line: $line)</p><p><b>$error</b>: $title</p></div><br /><div><p><b>Debug Backtrace &copy;Varimax</b></p><table cellpadding="8" cellspacing="1" bgcolor="#aaa" width="100%"><tbody>$backtrace</tbody></table></div></body></html>');
             }
         }
-        exit();
     }
 }
