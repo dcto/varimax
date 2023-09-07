5b783677899996ba02a87afa59b5fddfd9ecb41c
dc.To <sdoz@live.com>
2023-09-07 14:24:29 +0800

    update DatabaseServiceProvider

--- src/Services/DatabaseServiceProvider.php
+++ src/Services/DatabaseServiceProvider.php
@@ -10,22 +10,84 @@ namespace VM\Services;
  * Time: 2020-08-19 15:42
  * SITE: https://www.varimax.cn/
  */
-
+use VM\Model;
+use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Query\Builder;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Contracts\Queue\EntityResolver;
+use Illuminate\Database\Eloquent\QueueEntityResolver;
+use Illuminate\Database\Connectors\ConnectionFactory;
 
-class DatabaseServiceProvider extends \Illuminate\Database\DatabaseServiceProvider
+class DatabaseServiceProvider extends ServiceProvider
 {
+
     /**
-     * Register the service provider.
+     * Bootstrap the application events.
+     *
      * @return void
      */
-    public function register(){
-        parent::register();
-        $this->booted(function(){
-            $this->registerQueryEvents();
-            $this->registerNestCollect();
-            $this->registerDbQueryLogs();
+    public function boot()
+    {
+        Model::setConnectionResolver($this->app['db']);
+
+        Model::setEventDispatcher($this->app['events']);
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        Model::clearBootedModels();
+
+        $this->registerConnectionServices();
+        $this->registerQueueableEntityResolver();
+        $this->registerQueryEvents();
+        $this->registerNestCollect();
+        $this->registerDbQueryLogs();
+    }
+
+    /**
+     * Register the primary database bindings.
+     *
+     * @return void
+     */
+    protected function registerConnectionServices()
+    {
+        // The connection factory is used to create the actual connection instances on
+        // the database. We will inject the factory into the manager so that it may
+        // make the connections while they are actually needed and not of before.
+        $this->app->singleton('db.factory', function ($app) {
+            return new ConnectionFactory($app);
+        });
+
+        // The database manager is used to resolve various connections, since multiple
+        // connections might be managed. It also implements the connection resolver
+        // interface which may be used by other components requiring connections.
+        $this->app->singleton('db', function ($app) {
+            return new \Illuminate\Database\DatabaseManager($app, $app['db.factory']);
+        });
+
+        $this->app->bind('db.connection', function ($app) {
+            return $app['db']->connection();
+        });
+
+        $this->app->singleton('db.transactions', function ($app) {
+            return new \Illuminate\Database\DatabaseTransactionsManager;
+        });
+    }
+
+    /**
+     * Register the queueable entity resolver implementation.
+     *
+     * @return void
+     */
+    protected function registerQueueableEntityResolver()
+    {
+        $this->app->singleton(EntityResolver::class, function () {
+            return new QueueEntityResolver;
         });
     }
 
