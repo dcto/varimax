524d4f680b7e703d9a7a59fab6fba5b50e3b4b67
dc.To <sdoz@live.com>
2023-10-31 17:31:16 +0800

    rebuild the service provider add update defer

--- src/Services/ServiceProvider.php
+++ src/Services/ServiceProvider.php
@@ -5,4 +5,20 @@ namespace VM\Services;
 /**
 * @package Services
 */
-abstract class ServiceProvider extends \Illuminate\Support\ServiceProvider {}
+abstract class ServiceProvider extends \Illuminate\Support\ServiceProvider 
+{
+    /**
+     * deferred boot service provider
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Determine if the provider is deferred.
+     * @return bool
+     */
+    public function isDeferred()
+    { 
+        return $this->defer;
+    }
+}
