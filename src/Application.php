fe1e10da2dcf5a1f58505a38d64b7b0eab069d87
dc.To <sdoz@live.com>
2024-04-29 10:39:23 +0800

    update database service provider

--- src/Application.php
+++ src/Application.php
@@ -107,7 +107,7 @@ class Application extends \Illuminate\Container\Container
     private function registerServiceProviders()
     {
         array_map([$this, 'register'], array_merge([
-            \VM\Services\PaginationServiceProvider::class,
+            \Illuminate\Events\EventServiceProvider::class,
             \VM\Services\DatabaseServiceProvider::class,
         ], (array) $this->config['service']));
     }
--- src/Services/DatabaseServiceProvider.php
+++ src/Services/DatabaseServiceProvider.php
@@ -10,21 +10,19 @@ namespace VM\Services;
  * Time: 2020-08-19 15:42
  * SITE: https://www.varimax.cn/
  */
-use VM\Model;
-use Illuminate\Support\Collection;
+
 use Illuminate\Database\Query\Builder;
-use Illuminate\Events\EventServiceProvider;
+use \Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\DatabaseManager;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Contracts\Queue\EntityResolver;
+
 use Illuminate\Database\Eloquent\QueueEntityResolver;
 use Illuminate\Database\Connectors\ConnectionFactory;
+use Illuminate\Database\DatabaseTransactionsManager;
 
 class DatabaseServiceProvider extends ServiceProvider
 {
-    /**
-     * @var bool
-     */
-    protected $defer = true;
-
     /**
      * Bootstrap the application events.
      *
@@ -32,24 +30,14 @@ class DatabaseServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->app->register(EventServiceProvider::class);
-
-        $this->registerConnectionServices();
-        
-        $this->registerQueueableEntityResolver();
-
         $this->registerQueryEvents();
-
-        $this->registerDbQueryLogs();
 
         $this->registerNestCollect();
 
-        Model::clearBootedModels();
+        $this->registerQueryLogs();
         
-        Model::setConnectionResolver($this->app['db']);
+        $this->registerQueueableEntityResolver();
 
-        Model::setEventDispatcher($this->app['events']);
-        
         PaginationServiceProvider::paginator();
     }
 
@@ -60,14 +48,15 @@ class DatabaseServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->registerDatabaseServices();
+        $this->registerDatabasesResolver();
+        $this->registerConnectionServices();
+        $this->registerQueueableEntityResolver();
     }
 
-
     /**
-     * Register the database service to application
+     * Register the connection service to application
      */
-    protected function registerDatabaseServices()
+    protected function registerDatabasesResolver()
     {
         // The connection factory is used to create the actual connection instances on
         // the database. We will inject the factory into the manager so that it may
@@ -80,8 +69,19 @@ class DatabaseServiceProvider extends ServiceProvider
         // connections might be managed. It also implements the connection resolver
         // interface which may be used by other components requiring connections.
         $this->app->singleton('db', function ($app) {
-            return new \Illuminate\Database\DatabaseManager($app, $app['db.factory']);
+            return new DatabaseManager($app, $app['db.factory']);
         });
+
+        //set setEventDispatcher
+        if ($this->app->bound('events')) {
+            $this->app['db']->setEventDispatcher($this->app['events']);
+        }
+
+        //set setEventDispatcher
+        Model::setEventDispatcher($this->app['events']);
+
+        //set ConnectionResolver
+        Model::setConnectionResolver($this->app['db']);
     }
 
     /**
@@ -91,13 +91,12 @@ class DatabaseServiceProvider extends ServiceProvider
      */
     protected function registerConnectionServices()
     {
-
         $this->app->bind('db.connection', function ($app) {
             return $app['db']->connection();
         });
 
-        $this->app->singleton('db.transactions', function ($app) {
-            return new \Illuminate\Database\DatabaseTransactionsManager;
+        $this->app->singleton('db.transactions', function () {
+            return new DatabaseTransactionsManager;
         });
     }
 
@@ -118,27 +117,51 @@ class DatabaseServiceProvider extends ServiceProvider
      */
     protected function registerQueryExtends()
     {
-        /**
-         * toSql extend
-         */
         Builder::macro('getSql', function () {
-            $bindings = $this->getBindings();
-            $sql = str_replace('?', "'%s'", $this->toSql());
-            return sprintf($sql, ...$bindings);
+            /** @var Builder $this */
+            return sprintf(str_replace('?', "'%s'", $this->toSql()), ...$this->getBindings());
         });
 
-        /**
-         * whereDateTime extend
-         */
-        Builder::macro('atDate', function($column, $date, $symbol = '~'){
+        Model::macro('atDate', function($column, $date, $symbol = '~'){
             $date = strstr($date, $symbol) ? array_map('trim', explode($symbol, $date)): array($date, $date);
             $atDate[0] = \Carbon\Carbon::parse($date[0])->startOfDay()->toDateTimeString();
             $atDate[1] = \Carbon\Carbon::parse($date[1])->endOfDay()->toDateTimeString();
-
+             /** @var Builder $this */
             return $this->whereBetween($column, $atDate);
         });
 
     }
+
+    /**
+     * Register Query Events
+     * 
+     * @todo addition listen to query event
+     */
+    protected function registerQueryEvents()
+    {
+        $this->app['db']->beforeExecuting(function($query){
+            // $this->app['log']->debug('beforeExecuting: '. $query);
+        });
+
+
+    }
+
+    /**
+     * Register Query Logs
+     */
+    protected function registerQueryLogs()
+    {
+        $this->app['db']->listen(function($query) {
+            $log = "[$query->time] ".vsprintf(str_replace("?", "'%s'", $query->sql), $query->bindings);                
+            if($query->time > 200){
+                $this->app->log->dir('db.'. $query->connectionName, 'slow')->warning($log);
+            }
+            if($this->app->config->get('app.log') > 1){
+                $this->app->log->dir('db.'. $query->connectionName, _APP_)->debug($log);
+            }
+        });
+    }
+
 
     /**
      * The collection nested methods 
@@ -147,13 +170,22 @@ class DatabaseServiceProvider extends ServiceProvider
      * collection($array)->sub($pid=1);  //get collection children
      * collection($array)->subs($pid=1); //get collection childrens
      * collection($array)->get(['id', 'pid', 'name'])->subs($pid=1)->tree('children'); //build collection to tree
-     * 
      * @return array
      * @version 20230320
      */
     protected function registerNestCollect(){
-        Collection::macro('top', function($id, $col = 'id'){return $this->where($col, $id);});
+        
+        Collection::macro('top', function($id, $col = 'id'){ 
+            /** @var Collection $this */
+            return $this->where($col, $id);
+        });
+        Collection::macro('sub', function($id, $col = 'pid'){
+            /** @var Collection $this */
+            return $this->where($col, $id);
+        });
+
         Collection::macro('tops', function($id, $col = 'pid'){
+            /** @var Collection $this */
             $parents = collect([]);
             $parent = $this->where('id', $id)->first();
             while(!is_null($parent)) {
@@ -162,17 +194,19 @@ class DatabaseServiceProvider extends ServiceProvider
             }
             return $parents;
         });
-        Collection::macro('sub', function($id, $col = 'pid'){return $this->where($col, $id);});
+       
         Collection::macro('subs', function($id = 0, $col = 'pid'){
             $childs = collect([]);
-            $child = $this->where($col, $id);
+            /**
+             * @var Collection $this
+             */
+            $child = $this->where($col, $id)->toBase();// $child = $this->where($col, $id);
             while($child->count()){
                 $childs->push(...$child);
                 $child = $this->whereIn($col, $child->pluck('id'));
             }
             return $childs;
         });
-
 
         Collection::macro('toTree', function(string $child = 'subs', string $level = 'deep', string $parent = 'pid', string $keyBy = 'id'){
             $trees = [];
@@ -196,6 +230,7 @@ class DatabaseServiceProvider extends ServiceProvider
             * 鸣谢@kid 解决层级level问题。
             * @author  dc.To
             * @version 20231107
+            * @var Collection $this
             */
             $items = $this->keyBy($keyBy)->toArray();
             foreach($items as $item){
@@ -218,41 +253,7 @@ class DatabaseServiceProvider extends ServiceProvider
             //         $trees[$item[$keyBy]] = &$this[$item[$keyBy]];
             //     }
             // });
-
-
             return $trees;
         });
-    }
-
-    /**
-     * Register Query Events
-     * 
-     * @todo addition listen to query event
-     */
-    protected function registerQueryEvents()
-    {
-        /*
-        $this->app['db']->listen(function($query){
-
-        });
-        */
-    }
-
-    /**
-     * Register Query Logs
-     */
-    protected function registerDbQueryLogs()
-    {
-        if(getenv('ENV') || getenv('DEBUG') || $this->app['config']['app.log'] > 0){
-            $this->app['db']->listen(function($query) {
-                $sql = vsprintf(str_replace("?", "'%s'", $query->sql), $query->bindings);                
-                if($query->time > 500){
-                    $this->app['log']->dir('db.'. $query->connectionName, 'slow')->warning('['.$query->time.' ms] '.$sql);
-                }
-                if($this->app['config']['app.log'] > 1){
-                    $this->app['log']->dir('db.'. $query->connectionName, _APP_)->debug('['.$query->time.' ms] '.$sql);
-                }
-            });
-        }
     }
 }
