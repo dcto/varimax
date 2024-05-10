e69359cd11ff2f4b0c5dabe09fee594c0de1bc45
dc.To <sdoz@live.com>
2024-05-10 11:04:38 +0800

    add Context module

---
+++ src/Context.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Varimax The Slim PHP Frameworks.
+ * varimax
+ * FILE: 2024
+ * USER: 陶之11. <sdoz@live.com>
+ * Time: 2024-05-10 15:30:00
+ * SITE: https://www.varimax.cn/
+ */
+namespace VM;
+
+use Swoole\Coroutine;
+
+class Context
+{
+    protected static $pool = [];
+
+    static function get($key)
+    {
+        $cid = Coroutine::getuid();
+        if ($cid < 0)
+        {
+            return null;
+        }
+        if(isset(self::$pool[$cid][$key])){
+            return self::$pool[$cid][$key];
+        }
+        return null;
+    }
+
+    static function put($key, $item)
+    {
+        $cid = Coroutine::getuid();
+        if ($cid > 0)
+        {
+            self::$pool[$cid][$key] = $item;
+        }
+
+    }
+    
+    static function delete($key = null)
+    {
+        $cid = Coroutine::getuid();
+        if ($cid > 0)
+        {
+            if($key){
+                unset(self::$pool[$cid][$key]);
+            }else{
+                unset(self::$pool[$cid]);
+            }
+        }
+    }
+}
