3d029420e4d9fbd0779d99685a36e46e4ee2bd04
dc.To <sdoz@live.com>
2024-05-10 11:23:02 +0800

    add call function

--- bin/library.php
+++ bin/library.php
@@ -77,6 +77,30 @@ if(!function_exists('lang')) {
     }
 }
 
+if (! function_exists('call')) {
+    /**
+     * Call a callback with the arguments.
+     *
+     * @param mixed $callback
+     * @return null|mixed
+     */
+    function call($callback, array $args = [])
+    {
+        $result = null;
+        if ($callback instanceof \Closure) {
+            $result = $callback(...$args);
+        } elseif (is_object($callback) || (is_string($callback) && function_exists($callback))) {
+            $result = $callback(...$args);
+        } elseif (is_array($callback)) {
+            [$object, $method] = $callback;
+            $result = is_object($object) ? $object->{$method}(...$args) : $object::$method(...$args);
+        } else {
+            $result = call_user_func_array($callback, $args);
+        }
+        return $result;
+    }
+}
+
 if(!function_exists('route')) {
     /**
      * get the route
