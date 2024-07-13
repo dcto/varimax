0baf72ce35080fbd1532a2421751aa034992c254
dc.To <sdoz@live.com>
2024-07-13 17:58:17 +0800

    add Context

---
+++ src/Facades/Context.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * Class Captcha
+ * @method static mixed get(string $key)
+ * @method static void set(string $key, mixed $value)
+ * @method static void delete(string $key)
+ */
+class Context extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return \VM\Context::class;
+    }
+}
